a0b9e2e5674dbebbd0f3fe1503a08ea3dfe57e7d
CamiloCapador <juanka1307@gmail.com>
2023-09-01 19:38:17 -0500

    Commit inicial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    private static WebDriver driver;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Camilo\\IdeaProjects\\Test2\\src\\main\\resources\\chromedriver.exe");
+
+        //Arrancar el navegador
+        driver = new ChromeDriver();
+
+        //Abrir pagina
+        driver.get("https://www.imalittletester.com/");
+
+        //Maximizar el navegador
+        driver.manage().window().maximize();
     }
 }
